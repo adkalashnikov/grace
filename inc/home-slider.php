1f8b98daf00e62f30d77c765655baed00bc3cf1f
unknown <adkalashnikov@gmail.com>
2018-10-17 15:32:34 +0300

    add btn to home slide

--- inc/home-slider.php
+++ inc/home-slider.php
@@ -23,7 +23,11 @@
                     <?php the_title(); ?>
                 </h2>
 
-                <a href="#" class="btn-more">Заказать пошив</a>
+                <a href="<?php if( get_field('btn-link') ) {the_field('btn-link');} ?>"
+                   class="btn-more">
+                    <?php if( get_field('btn-text') ) {the_field('btn-text');} ?>
+                    <svg class="icon icon-arr-right"><use xlink:href="#icon-arr-right"></use></svg>
+                </a>
             </li>
 
                 <?php
